12fe36120fe9fec36ccb8b84e46624d0dfb1d938
wangchenmf <37495908+wangchenmf@users.noreply.github.com>
2019-03-17 13:43:08 +0800

    Update GitTest.java

--- demo/src/main/java/GitTest.java
+++ demo/src/main/java/GitTest.java
@@ -1,5 +1,5 @@
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Git Hub up first file !!!");
+        System.out.println("Git Hub up first file  and Tom !!!");
     }
 }
